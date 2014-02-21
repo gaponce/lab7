6b33673945e6963f468d72c9c5648a37a6eb6ce0
Gabriela Ponce <gaponce@eng.ucsd.edu>
2014-02-21 23:24:53 +0000

    submitting lab 7

--- routes/project.js
+++ routes/project.js
@@ -4,8 +4,11 @@ exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
 
   // query for the specific project and
-  // call the following callback
+  models.Project
+    .find({"_id": projectID})
+    .exec(afterQuery);
 
+  // call the following callback
   function afterQuery(err, projects) {
     if(err) console.log(err);
     res.json(projects[0]);
@@ -17,12 +20,37 @@ exports.addProject = function(req, res) {
   console.log(form_data);
 
   // make a new Project and save it to the DB
+  var newPost = new models.Project({
+    "title": form_data["project_title"],
+    "date": form_data["date"],
+    "summary": form_data["summary"],
+    "image": form_data["image_url"]
+  });
+
+  newPost.save(afterSaving);
+
   // YOU MUST send an OK response w/ res.send();
+  function afterSaving(err) {
+    if (err) {console.log(err); res.send(500);}
+
+      res.send();
+
+      //redirect to home
+      res.redirect('/');
+  }
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
   // find the project and remove it
+  models.Project
+    .find({"_id": projectID})
+    .remove()
+    .exec(afterRemoving);
+
   // YOU MUST send an OK response w/ res.send();
+  function afterRemoving(err){
+    res.send();
+  }
 }
